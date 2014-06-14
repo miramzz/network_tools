6dcd3abf8045db295b5ac3dd6030041d7520a73c
miramzz <mira.muazzez@gmail.com>
2014-06-14 01:13:57 -0700

    buffersize test case is added, server file is changed for multiprocessing option, buffersizes fixed to 32 in all files

--- test_echo.py
+++ test_echo.py
@@ -2,18 +2,24 @@ import pytest
 import socket
 from os import urandom
 from echo_client import echo_client
-from echo_server import echo_server
 
 
 
 def test_echo():
-    #need to awake echo_server here but i couldn't figure out how
+    assert echo_client('Test message') == 'Test message'
+
+def test_random():
     test1 = urandom(100)
-    assert echo_client(u"Test message") == u"Test message"
     assert echo_client(test1) == test1
-    assert echo_client() == u"Enter a message to be sent"
 
+def test_string():
+    assert echo_client(u"Enter a message to be sent") == 'Enter a message to be sent'
 
+def test_long():
+    long_msg = b'\x00'*31
+    assert echo_client(long_msg) == long_msg
+
+def test_error():
     with pytest.raises(socket.error):
         echo_client(u"Eot test is done")
 
