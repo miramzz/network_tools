271aff32ffb9f4dc4579c1140d1a8419db8919c5
miramzz <mira.muazzez@gmail.com>
2014-06-14 15:41:06 -0700

    buffersize increased to 4096, function description is added, print out messages type changed to byte, exceptions are specified, return value cast to bytearray

--- echo_client.py
+++ echo_client.py
@@ -4,21 +4,24 @@ import sys
 
 
 def echo_client(message) :
+    """Creates a connection on the local host
+        and sends back the received message
+    """
     try :
         my_socket = socket.socket(
             socket.AF_INET,socket.
             SOCK_STREAM,socket.IPPROTO_IP)
     except socket.error:
-        print u"Failed to create socket"
+        print 'Failed to create socket'
         sys.exit()
     try :
-        my_socket.connect(('127.0.0.1', 50000))
+        my_socket.connect(('127.0.0.1', 5000))
     except :
-        print u"Failed to create a connection"
+        print 'Failed to make a connection'
         sys.exit()
 
     my_socket.sendall(message)
-    buffsize = 32
+    buffsize = 4096
     tmp_msg = ''
     while True :
         recv_msg = my_socket.recv(buffsize)
@@ -27,16 +30,16 @@ def echo_client(message) :
             break
     try :
         my_socket.close()
-    except :
-        print u"Connection still alive"
-    return unicode(tmp_msg, "UTF-8")
+    except socket.error:
+        print 'Connection is still alive'
+    return bytearray(tmp_msg)
 
 
 if __name__ == "__main__":
     try :
-        data = sys.argv[1].encode('UTF-8')
+        data = sys.argv[1]
     except IndexError :
-        print u"Enter a message to be sent"
-    print echo_client(data)
+        print 'Enter a message to send'
+    print echo_client(bytearray(data))
 
 
