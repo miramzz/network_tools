c61587c5c5152f458ef46a578b0ef4620038e633
miramzz <mira.muazzez@gmail.com>
2014-06-16 01:45:06 -0700

    sever and client are added to gevent

--- gevent_server.py
+++ gevent_server.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
+import socket
+import http_header as hh
 
-def echo(address, socket):
-    buffsize = 4096
+buffsize = 4096
+
+
+def server(conn):
+    my_msg = ''
     while True:
-        data = socket.recv(buffsize)
-        if data:
-            socket.sendall(data)
-        else:
-            socket.close()
+        chunk = conn.recv(buffsize)
+        if (len(chunk) < buffsize):
+            my_msg += chunk
             break
+    conn.shutdown(socket.SHUT_RD)
+    conn.sendall(hh.create_response(my_msg))
+    conn.shutdown(socket.SHUT_WR)
+    conn.close()
+
+def client(address) :
+    my_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_IP
+            )
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.bind(address)
+    my_socket.listen(1)
+    my_response = server(my_socket.accept())
+    my_response.close()
+    return my_response
+
 
 if __name__=="__main__":
     from gevent.server import StreamServer
     from gevent.monkey import patch_all
     patch_all()
-    server = StreamServer(('127.0.0.1', 5000), echo)
-    server.serve_forever()
-
+    server_ = StreamServer(('127.0.0.1', 5000), client)
+    server_.serve_forever()
