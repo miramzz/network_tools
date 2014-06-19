de1bb82e5cc0a1c9842e0e60aacbb7e2de984567
miramzz <mira.muazzez@gmail.com>
2014-06-18 22:21:07 -0700

    code is now stabil

--- echo_server.py
+++ echo_server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import socket
 import sys
-from multiprocessing import Process
-import echo_client as ec
 
 
 def echo_server():
@@ -19,30 +17,22 @@ def echo_server():
 
     my_socket.bind(address)
     my_socket.listen(1)
-
-    while True:
-        conn, client_add = my_socket.accept()
-        my_msg = ''
+    try:
         while True:
-            recv_msg = conn.recv(buffsize)
-            my_msg += recv_msg
-            if (len(recv_msg) < buffsize):
-                print recv_msg
-                break
-        conn.shutdown(socket.SHUT_RD)
-        conn.sendall(my_msg)
-        conn.shutdown(socket.SHUT_WR)
-    conn.close()
-    my_socket.close()
+            conn, client_add = my_socket.accept()
+            my_msg = ''
+            while True:
+                recv_msg = conn.recv(buffsize)
+                my_msg += recv_msg
+                if (len(recv_msg) < buffsize):
+                    print recv_msg
+                    break
+            conn.shutdown(socket.SHUT_RD)
+            conn.sendall(my_msg)
+            conn.shutdown(socket.SHUT_WR)
+            conn.close()
+    except KeyboardInterrupt:
+        my_socket.close()
 
 if __name__ == "__main__":
-    try:
-        data = sys.argv[1]
-    except IndexError:
-        echo_server()
-    else:
-        ec.create_conn()
-        p = Process(target=echo_server)
-        p.start()
-        p.join()
-        ec.send_msg(bytearray(data))
+    echo_server()
