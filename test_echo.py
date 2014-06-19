3a1d4475ee2b9d0f1cc7ac94d038aa3a34e78b20
miramzz <mira.muazzez@gmail.com>
2014-06-18 21:45:11 -0700

    notation problems fixed

--- test_echo.py
+++ test_echo.py
@@ -4,23 +4,25 @@ from os import urandom
 from echo_client import echo_client
 
 
-
 def test_echo():
     assert echo_client('Test message') == 'Test message'
+
 
 def test_random():
     test1 = urandom(100)
     assert echo_client(test1) == test1
 
+
 def test_string():
-    assert echo_client(u"Enter a message to be sent") == 'Enter a message to be sent'
+    assert echo_client(u"Enter a message to be sent") == \
+        'Enter a message to be sent'
+
 
 def test_long():
     long_msg = b'\x00'*31
     assert echo_client(long_msg) == long_msg
 
+
 def test_error():
     with pytest.raises(socket.error):
         echo_client(u"Eot test is done")
-
-
