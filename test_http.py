1e2fed736c35e87bc223762743089e04608e5bba
miramzz <mira.muazzez@gmail.com>
2014-06-18 23:59:37 -0700

    notation problems fixed and removed extra files

--- test_http.py
+++ test_http.py
@@ -1,4 +1,5 @@
-from http_server import create_uri_request
+from http_server import create_response
+
 
 def http_msg(msg_type):
     date_time = "2014-06-12 16:39:06.162234"
@@ -22,45 +23,40 @@ def test_200_OK():
 GET / HTTP/1.1\r\n
 Host: www.example.com\r\n\r\n
 """)
-    assert create_uri_request(msg) == http_msg('200 OK')
+    assert create_response(msg) == http_msg('200 OK')
 
-# method test if we send PUT instead of GET
+
 def test_get():
     msg = b"""\
 PUT / HTTP/1.1\r\n
 Host: www.example.com\r\n\r\n
 <CRLF>"
 """
-    assert create_uri_request(msg) == http_msg('405 Method Not Allowed')
+    assert create_response(msg) == http_msg('405 Method Not Allowed')
 
-# uri test if we send not root
+
 def test_uri():
     msg = b"""\
 GET /index.html HTTP/1.1\r\n
 Host: www.example.com\r\n\r\n
 <CRLF>"
 """
-    assert create_uri_request(msg) == http_msg('405 Method Not Allowed')
+    assert create_response(msg) == http_msg('405 Method Not Allowed')
 
-# protocol test if we send 1.0
+
 def test_protocol():
     msg = b"""\
 GET / HTTP/1.0\r\n
 Host: www.example.com\r\n\r\n
 <CRLF>"
 """
-    assert create_uri_request(msg) == http_msg('400 Bad Request')
+    assert create_response(msg) == http_msg('400 Bad Request')
 
 
-# protocol test if we don't send http/1.1
 def test_protocol2():
     msg = b"""\
 GET / HTP\r\n
 Host: www.example.com\r\n\r\n
 <CRLF>"
 """
-    assert create_uri_request(msg) == http_msg('400 Bad Request')
-
-
-
-
+    assert create_response(msg) == http_msg('400 Bad Request')
