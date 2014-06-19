95b21e2b7fd18386767ba25dc7da6d12b3ca3d8c
miramzz <mira.muazzez@gmail.com>
2014-06-18 23:50:10 -0700

    Key interruption added

--- http_server.py
+++ http_server.py
@@ -5,20 +5,21 @@ import socket
 
 # sudo lsof -n | grep LISTEN
 
-err_tags = {400:'Bad Request',
-            403:'Forbidden',
-            404:'Page Not Found',
-            405:'Method Not Allowed',
-            408:'Request Timeout',
-            429:'Too Many Requests',
-            444:'No Response',
-            500:'Internal Server Error',
-            200:'OK'}
+err_tags = {400: 'Bad Request',
+            403: 'Forbidden',
+            404: 'Page Not Found',
+            405: 'Method Not Allowed',
+            408: 'Request Timeout',
+            429: 'Too Many Requests',
+            444: 'No Response',
+            500: 'Internal Server Error',
+            200: 'OK'}
+
 
 def create_http_response(msg_type):
     # this line is for testing
     date_time = '2014-06-12 16:39:06.162234'
-    #date_time = datetime.datetime.now()
+    # date_time = datetime.datetime.now()
     byte_string = """\
 HTTP/1.1 {}\r\n
 Date: {}\r\n
@@ -32,19 +33,23 @@ Content-Type: text/html; charset=UTF-8\r\n\r\n
 """.format(msg_type, date_time)
     return bytearray(byte_string)
 
+
 def check_request_method(method):
     if method.upper() != 'GET':
         return 405
+
 
 def check_request_uri(uri):
     if uri != "/":
         return 405
 
+
 def check_request_protocol(protocol):
     if protocol.upper() != 'HTTP/1.1':
         return 400
 
-def check_err_response(method,uri,protocol,host):
+
+def check_err_response(method, uri, protocol, host):
     err_code = 0
     if check_request_method(method):
         err_code = check_request_method(method)
@@ -53,9 +58,10 @@ def check_err_response(method,uri,protocol,host):
     elif check_request_protocol(protocol):
         err_code = check_request_protocol(protocol)
 
-    if err_tags.has_key(err_code) :
+    if err_code in err_tags:
         err_message = "{} {}".format(err_code, err_tags.get(err_code))
         raise NameError(err_message)
+
 
 def create_response(recv_msg):
     recv_msg = recv_msg.split('\r\n')
@@ -76,8 +82,7 @@ def create_response(recv_msg):
 
 def echo_server():
     my_socket = socket.socket(
-    socket.AF_INET,socket.
-    SOCK_STREAM,socket.IPPROTO_IP)
+        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
 
     address = ('127.0.0.1', 5000)
     buffsize = 4096
@@ -86,24 +91,22 @@ def echo_server():
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.bind(address)
     my_socket.listen(1)
-    while True :
-        my_msg = ''
-        conn, client_add = my_socket.accept()
-        while  True:
-            recv_msg = conn.recv(buffsize)
-            my_msg += recv_msg
-            if (len(recv_msg) < buffsize):
-                break
-        conn.shutdown(socket.SHUT_RD)
-        conn.sendall(create_response(my_msg))
-        conn.shutdown(socket.SHUT_WR)
-        conn.close()
+    try:
+        while True:
+            my_msg = ''
+            conn, client_add = my_socket.accept()
+            while True:
+                recv_msg = conn.recv(buffsize)
+                my_msg += recv_msg
+                if (len(recv_msg) < buffsize):
+                    break
+            conn.shutdown(socket.SHUT_RD)
+            conn.sendall(create_response(my_msg))
+            conn.shutdown(socket.SHUT_WR)
+            conn.close()
+    except KeyboardInterrupt:
+        my_socket.close()
 
-    my_socket.close()
-
-if __name__=="__main__":
+if __name__ == "__main__":
     echo_server()
-    #print create_uri_request(b"GET /path/to/index.html HTTP/1.1\r\nHost: www.mysite1.com:80")
-
-
-
+    # print create_uri_request(b"GET /path/to/index.html HTTP/1.1\r\nHost: www.mysite.com:80")
