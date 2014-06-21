d393eacd4e8e98fa6be239d589353c6ca21df000
miramzz <mira.muazzez@gmail.com>
2014-06-21 15:30:00 -0700

    notation problem fixed

--- http_server.py
+++ http_server.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import socket
 import http_header as hh
-#import pdb; pdb.set_trace()
+
 
 def echo_server():
-    try :
+    try:
         my_socket = socket.socket(
             socket.AF_INET,
             socket.SOCK_STREAM,
@@ -19,13 +19,13 @@ def echo_server():
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.bind(address)
     my_socket.listen(1)
-    while True :
+    while True:
         my_msg = ''
-        try :
+        try:
             conn, client_add = my_socket.accept()
-        except  socket.error:
+        except socket.error:
             print 'Unable to make a connection'
-        while  True:
+        while True:
             recv_msg = conn.recv(buffsize)
             my_msg += recv_msg
             print recv_msg, len(recv_msg)
@@ -39,9 +39,8 @@ def echo_server():
 
     my_socket.close()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     echo_server()
-    # print hh.create_uri_request(b"GET / HTTP/1.1\r\nHost: www.mysite1.com:80")
-    #print hh.create_uri_request(b"GET /Users/muazzezmira/Desktop/webroot HTTP/1.1\r\nHost: www.mysite1.com:80")
-
-
+# print hh.create_uri_request(b"GET / HTTP/1.1\r\nHost: www.mysite1.com:80")
+# print hh.create_uri_request(b"GET /Users/muazzezmira/Desktop/
+# webroot HTTP/1.1\r\nHost: www.mysite1.com:80")
