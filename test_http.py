66d8e1dc0ef2d45448e965e79b37d275ce9d75dc
miramzz <mira.muazzez@gmail.com>
2014-06-21 20:51:16 -0700

    it seems like it is failing but it is passing actually

--- test_http.py
+++ test_http.py
@@ -1,7 +1,7 @@
-from http_server import create_uri_request
+from http_header import create_response
 
 
-def http_msg(msg_type):
+def http_msg(msg_type, cont_len, cont_type, content):
     date_time = "2014-06-12 16:39:06.162234"
     byte_string = b"""\
 HTTP/1.1 {}\r\n\
@@ -10,60 +10,58 @@ Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n\
 Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n\
 Etag: "3f80f-1b6-3e1cb03b"\r\n\
 Accept-Ranges:  none\r\n\
-Content-Length: 438\r\n\
+Content-Length: {}\r\n\
 Connection: close\r\n\
-Content-Type: text/html; charset=UTF-8\r\n\r\n\
-""".format(msg_type, date_time)
+Content-Type: {}; charset=UTF-8\r\n\r\n\
+{}
+""".format(msg_type, date_time, cont_len, cont_type, content)
     return byte_string
 
 
-# test if it is working
 def test_200_OK():
-    msg = """\
+    # test if it is working
+    msg = b"""\
 GET / HTTP/1.1\r\n\
-Host: www.example.com\r\n\
-<CRLF>"
+Host: www.example.com\r\n\r\n\
 """
-    assert create_uri_request(bytearray(msg)) == http_msg("200 OK")
+    assert create_response(msg) == http_msg('200 OK', 0, 'None', '')
 
 
 def test_get():
     # method test if we send PUT instead of GET
-    msg = """\
+    msg = b"""\
 PUT / HTTP/1.1\r\n\
 Host: www.example.com\r\n\
-<CRLF>"
 """
-    assert create_uri_request(bytearray(msg)) == http_msg(
-        "405 Method Not Allowed")
+    assert create_response(msg) == http_msg(
+        '405 Method Not Allowed', 0, 'None', '')
 
 
 def test_uri():
     # uri test if we send not root
-    msg = """\
+    msg = b"""\
 GET /index.html HTTP/1.1\r\n\
 Host: www.example.com\r\n\
-<CRLF>"
 """
-    assert create_uri_request(bytearray(msg)) == http_msg(
-        "405 Method Not Allowed")
+    assert create_response(msg) == http_msg(
+        "405 Method Not Allowed", 0, 'None', '')
 
 
 def test_protocol():
     # protocol test if we send 1.0
-    msg = """\
+    msg = b"""\
 GET / HTTP/1.0\r\n\
 Host: www.example.com\r\n\
-<CRLF>"
 """
-    assert create_uri_request(bytearray(msg)) == http_msg("400 Bad Request")
+    assert create_response(msg) == http_msg(
+        "400 Bad Request", 0, 'None', '')
 
 
 def test_protocol2():
     # protocol test if we don't send http/1.1
-    msg = """\
+    msg = b"""\
 GET / HTP\r\n\
 Host: www.example.com\r\n\
-<CRLF>"
 """
-    assert create_uri_request(bytearray(msg)) == http_msg("400 Bad Request")
+    assert create_response(msg) == http_msg(
+        "400 Bad Request", 0, 'None', '')
